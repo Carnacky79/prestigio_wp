a6f63e3fdcf6094d5eadbadd8adc0dcdeffdb644
Carnacky79 <f.lombardi79@gmail.com>
2022-03-30 10:00:37 +0200

    Lettura file excel e scrittura dati in file di testo

--- src/main/java/ExcelClass.java
+++ src/main/java/ExcelClass.java
@@ -8,17 +8,10 @@ import java.util.*;
 import static org.apache.poi.ss.usermodel.Cell.*;
 
 public class ExcelClass {
+    private File exelFile;
     public ExcelClass(String file) throws IOException {
-        File myFile = new File(file);
-        FileInputStream fis = new FileInputStream(myFile);
+        this.exelFile = new File(file);
 
-        // Finds the workbook instance for XLSX file
-        XSSFWorkbook myWorkBook = new XSSFWorkbook (fis);
-
-        // Return first sheet from the XLSX workbook
-        XSSFSheet mySheet = myWorkBook.getSheetAt(0);
-
-        System.out.println(myWorkBook.getNumberOfSheets());
 /*
         // Get iterator to all the rows in current sheet
         Iterator<Row> rowIterator = mySheet.iterator();
@@ -49,5 +42,63 @@ public class ExcelClass {
             }
             System.out.println("");
         }*/
+    }
+
+    public HashMap<String, ArrayList<String[]>> leggiFogli() throws IOException {
+        HashMap<String,  ArrayList<String[]>> fogli = new HashMap<>();
+
+        FileInputStream fis = new FileInputStream(this.exelFile);
+
+        // Finds the workbook instance for XLSX file
+        XSSFWorkbook myWorkBook = new XSSFWorkbook (fis);
+
+        for(int i=0;i<myWorkBook.getNumberOfSheets();i++){
+            XSSFSheet myS = myWorkBook.getSheetAt(i);
+            String sheetName = myS.getSheetName();
+            ArrayList<String[]> righe = null;
+
+            // Get iterator to all the rows in current sheet
+            Iterator<Row> rowIterator = myS.iterator();
+
+            righe = new ArrayList<>();
+            // Traversing over each row of XLSX file
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                String[] celleRiga = new String[6];
+
+
+                // For each row, iterate through each columns
+                Iterator<Cell> cellIterator = row.cellIterator();
+                int c = 0;
+                while (cellIterator.hasNext()) {
+
+                    Cell cell = cellIterator.next();
+
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            celleRiga[c] = cell.getStringCellValue();
+                            break;
+                        case NUMERIC:
+                            celleRiga[c] = Double.toString(cell.getNumericCellValue());
+                            break;
+                        case BOOLEAN:
+                            celleRiga[c] = Boolean.toString(cell.getBooleanCellValue());
+                            break;
+                        default :
+
+                    }
+                    c++;
+                }
+
+            righe.add(celleRiga);
+
+            }
+
+            fogli.put(sheetName,righe);
+
+        }
+
+
+        return fogli;
     }
 }
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,11 +1,28 @@
 import java.io.*;
 import java.nio.file.*;
+import java.util.*;
 
 public class MainClass {
     public static void main(String[] args) throws IOException {
         String canonicalPath = new File(".").getCanonicalPath() + "\\listino.xlsx";
+        String outputFile = new File(".").getCanonicalPath() + "\\testout.txt";
+        HashMap<String, ArrayList<String[]>> fogli = new HashMap<>();
 
         ExcelClass excel = new ExcelClass(canonicalPath);
+
+        fogli = excel.leggiFogli();
+
+        FileWriter fw = new FileWriter(outputFile, true);
+        for(ArrayList<String[]> s : fogli.values()){
+            for(String[] c : s){
+                String riga = String.join("|", c);
+                fw.write(riga);
+                fw.write('\n');
+            }
+            fw.write("-------------------------------------");
+        }
+
+        fw.close();
 
     }
 }
